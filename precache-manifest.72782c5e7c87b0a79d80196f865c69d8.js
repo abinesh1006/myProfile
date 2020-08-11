0805441e34feec592e906e77fe98a0f172787278
abinesh subramani <abineshsiva191@gmail.com>
2020-08-11 13:10:01 +0530

    Updates

---
+++ precache-manifest.72782c5e7c87b0a79d80196f865c69d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662d3760eb38cdb28e2b13ea52504b65",
+    "url": "/myProfile/index.html"
+  },
+  {
+    "revision": "d33c5e001b7b174bc1cd",
+    "url": "/myProfile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99c7189615926838947f",
+    "url": "/myProfile/static/js/2.77c379de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myProfile/static/js/2.77c379de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d33c5e001b7b174bc1cd",
+    "url": "/myProfile/static/js/main.e902d5af.chunk.js"
+  },
+  {
+    "revision": "a4d0d98300bd0508b5d3",
+    "url": "/myProfile/static/js/runtime-main.6e467714.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myProfile/static/media/logo.5d5d9eef.svg"
+  }
+]);
